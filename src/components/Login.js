b7d0cd989f3c15b8984917935e507496e788a16a
Saiful Islam (Solar ICT) <129642915+saifulheig@users.noreply.github.com>
2024-08-26 20:05:59 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setMessage("");
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://otp-api.labontest.tech:5000/api/auth/login",
         { email, password }
       );
       setMessage(response.data.msg);
@@ -35,7 +35,7 @@ const Login = () => {
     setMessage("");
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/verify-login-otp",
+        "https://otp-api.labontest.tech:5000/api/auth/verify-login-otp",
         { email, otp }
       );
       setMessage(response.data.msg);
@@ -50,7 +50,7 @@ const Login = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/api/auth/google";
+    window.location.href = "https://otp-api.labontest.tech:5000/api/auth/google";
   };
 
   return (
